6206c2bd2b0d8ebda5de203319e29e02f7b3a0f5
Archana <archanasreenivas.k@gmail.com>
2023-08-10 13:43:16 +0530

    made chnages

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,15 +10,16 @@ module.exports = {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken:
-          process.env.DATO_READONLY_API_KEY || '1b2fe1bfc8abf2e08343a75e444b4d',
+          process.env.DATO_READONLY_API_KEY 
+          // || '1b2fe1bfc8abf2e08343a75e444b4d',
       },
     },
     {
       resolve: 'gatsby-plugin-snipcart-advanced',
       options: {
         publicApiKey:
-          process.env.SNIPCART_PUBLIC_API_KEY ||
-          'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
+          process.env.SNIPCART_PUBLIC_API_KEY,
+          //'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1'//
         autopop: true,
       },
     },
